2c69f7379136223b506baff75d058c5658322564
Ruslan <krasiyruslan@gmail.com>
2023-01-30 01:45:24 +0100

    edit mask

--- src/pages/bubble.js
+++ src/pages/bubble.js
@@ -4,7 +4,7 @@ export default function Bubble() {
 	return (
 		<div className="App">
 			<BubbleCanvas />
-			<img src="./mask2.png" className="fixed top-0 left-0 w-full h-full"  alt="mask"/>
+			<img src="./mask2a.png" className="fixed top-0 left-0 w-full h-full"  alt="mask"/>
 		</div>
 	);
 }
--- src/pages/mouse-meteor.js
+++ src/pages/mouse-meteor.js
@@ -4,7 +4,7 @@ export default function MouseMeteor() {
 	return (
 		<div className="App">
 			<MouseCanvas />
-			<img src="./mask2.png" className="fixed top-0 left-0 w-full h-full" alt="mask"/>
+			<img src="./mask2a.png" className="fixed top-0 left-0 w-full h-full" alt="mask"/>
 		</div>
 	);
 }
